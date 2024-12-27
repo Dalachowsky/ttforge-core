b507ea3ae7065311f083f7dcc18831269a5c3c8c
Kacper Dalach <dalachowsky@gmail.com>
2024-12-27 17:55:17 +0100

    test system

--- src/ttforge/system/system.py
+++ src/ttforge/system/system.py
@@ -5,6 +5,7 @@ from ttforge.core.registry import RegistryDict
 from ttforge.core.characteristic import CharacteristicPrimary, CharacteristicDerivedBase
 from ttforge.core.registry import RegistryMain
 from ttforge.core.registry.registry_main import RegistryMainEntryType
+from ttforge.core.resourcepool import ResourcePoolBase
 from ttforge.core.skill import SkillBase
 
 class TTForgeSystemRegistries:
@@ -12,6 +13,7 @@ class TTForgeSystemRegistries:
     CHARACTERISTICS = RegistryDict(CharacteristicPrimary)
     CHARACTERISTICS_DERIVED = RegistryDict(CharacteristicDerivedBase)
     SKILLS = RegistryDict(SkillBase)
+    RESOURCE_POOLS = RegistryDict(ResourcePoolBase)
 
 class TTForgeSystem:
 
@@ -32,3 +34,7 @@ class TTForgeSystem:
         # TODO raise errors
         self.registry.MAIN.register(skill.REGISTRY_ID, skill, RegistryMainEntryType.SKILL)
         self.registry.SKILLS.register(skill.REGISTRY_ID, skill)
+
+    def registerResourcePool(self, pool: type[ResourcePoolBase]):
+        self.registry.MAIN.register(pool.REGISTRY_ID, pool, RegistryMainEntryType.RESOURCE_POOL)
+        self.registry.RESOURCE_POOLS.register(pool.REGISTRY_ID, pool)
---
+++ tests/integration/test_system.py
@@ -0,0 +1,56 @@
+
+import pytest
+
+from ttforge.core.exception import DuplicateEntry
+from ttforge.core.resourcepool.resource_pool_base import ResourcePoolType
+from ttforge.system import TTForgeSystem
+from ttforge.core.resourcepool import ResourcePoolBase, resourcePool
+from ttforge.core.characteristic import CharacteristicPrimary, characteristicPrimary, CharacteristicDerivedBase, characteristicDerived
+from ttforge.core.skill import SkillBase, skill
+
+NS = "test"
+
+def test_register_each_type():
+    @characteristicPrimary(NS)
+    class MockCharacteristic(CharacteristicPrimary):
+        NAME = "Mock"
+
+    @characteristicDerived(NS)
+    class MockModifier(CharacteristicDerivedBase):
+        NAME = "Mock modifier"
+        DEPENDENCIES = ["test:mock"]
+
+        def _recalculate(self):
+            return self.getDep("test:mock") / 10
+
+    @skill(NS)
+    class MockSkill(SkillBase):
+        NAME = "Mocking"
+
+    @resourcePool(NS)
+    class HealthPool(ResourcePoolBase):
+        NAME = "Health points"
+
+        def __init__(self, value: ResourcePoolType, maxval: ResourcePoolType) -> None:
+            super().__init__(value, maxval)
+
+    sys = TTForgeSystem()
+    sys.registerCharacteristicPrimary(MockCharacteristic)
+    sys.registerCharacteristicDerived(MockModifier)
+    sys.registerSkill(MockSkill)
+    sys.registerResourcePool(HealthPool)
+
+def test_duplicate_registry_ID_different_type():
+
+    @characteristicPrimary(NS)
+    class MockCharacteristic(CharacteristicPrimary):
+        NAME = "Mock"
+
+    @skill(NS)
+    class MockSkill(SkillBase):
+        NAME = "Mock"
+
+    sys = TTForgeSystem()
+    sys.registerCharacteristicPrimary(MockCharacteristic)
+    with pytest.raises(DuplicateEntry):
+        sys.registerSkill(MockSkill)
